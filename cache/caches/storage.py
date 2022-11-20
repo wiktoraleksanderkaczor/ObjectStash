45eabedcfb8271f2cb75da8e57415b4bfab086d1
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2022-11-20 22:34:06 +0000

    Update storage cache model

--- cache/caches/storage.py
+++ cache/caches/storage.py
@@ -2,18 +2,21 @@ from typing import List
 
 from cache.models.cache import Cache
 from cache.models.policy import Policy
-from storage.client.models.objects import Object, ObjectID
+from storage.client.models.objects import Object, ObjectID, ObjectInfo
 
 
 class Storage(Cache):
     def __init__(self, wrapped: object, policy: Policy):
         super().__init__(wrapped, policy)
 
-    def get(self, key: ObjectID) -> Object:
+    def container_exists(self) -> bool:
         ...
 
-    def exists(self, key: ObjectID) -> bool:
+    def get_object(self, key: ObjectID) -> Object:
         ...
 
-    def items(self) -> List[ObjectID]:
+    def stat_object(self, key: ObjectID) -> ObjectInfo:
+        ...
+
+    def list_objects(self, prefix: ObjectID, recursive: bool = False) -> List[ObjectID]:
         ...
