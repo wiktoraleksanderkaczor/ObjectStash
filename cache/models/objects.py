c368a5cfe349a905ad7dbd05b64ded4d7d4d3a21
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2022-11-20 22:34:53 +0000

    Fill out cache data models

--- cache/models/objects.py
+++ cache/models/objects.py
@@ -1,16 +1,19 @@
-class CacheKey:
+from pathlib import PurePosixPath as CacheID
+
+from pydantic import BaseModel, Extra
+
+
+# Allow extra for arbitrary kwarg fields, or inner basemodel
+class CacheData(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+
+class CacheInfo(BaseModel):
     pass
 
 
-class CacheData:
-    pass
-
-
-class CacheInfo:
-    pass
-
-
-class CacheObject:
-    name: CacheKey
+class CacheObject(BaseModel):
+    name: CacheID
     info: CacheInfo
     data: CacheData
