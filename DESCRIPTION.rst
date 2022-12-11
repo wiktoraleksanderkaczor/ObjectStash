ef3047bc0bb57fd5ee4084889f2d1fab5861b1e7
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2022-12-11 19:58:19 +0000

    Update description and package fixes

---
+++ DESCRIPTION.rst
@@ -0,0 +1,8 @@
+ObjectStash is a serverless distribution-capable database, caching, and FUSE layer self-contained package.
+
+What can it do:
+- It can act like a filesystem, mounted wherever needed for interfacing with legacy applications. 
+- It is also a storage client abstraction option; cloud, memory block or API.
+- It can run a serverless multi-paradigm database on any storage client with online compute capabilities.
+- It allows masterless peer-to-peer coordination for different distribution strategies.
+- All of this with the ability to cache, replicate and shard as desired on any of the storage clients.
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
 from setuptools import find_packages
 
-DESCRIPTION = "Timeseries, NoSQL and Graph database exclusively on S3 or other compatible storage"
+DESCRIPTION = (
+    "Multi-paradigm database, caching and FUSE layer on various storage clients, for example, S3, memory, local etc."
+)
 setuptools.setup(
     name="ObjectStash",
     version="0.1.0",
@@ -10,13 +12,11 @@ setuptools.setup(
     author_email="wiktoraleksanderkaczor@gmail.com",
     description=DESCRIPTION,
     long_description=open("DESCRIPTION.rst").read(),
-    packages=find_packages,
-    package_data={"": ["*.yaml"]},
+    packages=find_packages(exclude=("tests")),
     install_requires=["minio", "zeroconf", "pysyncobj", "pyfuse", "pydantic", "PyYAML"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
-    include_package_data=True,
 )
