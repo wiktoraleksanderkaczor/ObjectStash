4eb4598b95bf5a7153218827721b0b6a1babac83
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2022-12-27 22:11:41 +0000

    Clean init

--- cache/__init__.py
+++
@@ -1,4 +0,0 @@
-from cache.wrapper.database import Database
-from cache.wrapper.storage import Storage
-
-clients = {"Partition": Database, "Storage": Storage}
--- storage/__init__.py
+++
@@ -1,5 +0,0 @@
-from storage.client.local import LocalClient
-from storage.client.memory import MemoryClient
-from storage.client.minio import MinIOClient
-
-clients = {"Local": LocalClient, "MinIO": MinIOClient, "Memory": MemoryClient}
