ee22946050c1cbedafef9a89f875f2440ff79d67
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-03-15 01:51:29 +0000

    Move locking models and add serialization

--- storage/models/wrapper/locking.py
+++ storage/models/wrapper/locking.py
@@ -1,4 +1,5 @@
 """Models for locking."""
+import pickle
 from datetime import datetime, timedelta
 
 from pydantic import BaseModel, Field
@@ -6,7 +7,14 @@ from pydantic import BaseModel, Field
 from config.env import env
 
 
-class LockState(BaseModel):
+class Lock(BaseModel):
     cluster: str = env.cluster.name
     timestamp: datetime = Field(default_factory=datetime.utcnow)
     duration: timedelta = env.locking.duration
+
+    def to_bytes(self) -> bytes:
+        return pickle.dumps(self, protocol=pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def from_bytes(data: bytes) -> "Lock":
+        return pickle.loads(data)
