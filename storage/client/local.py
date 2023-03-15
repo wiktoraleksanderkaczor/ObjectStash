bf2ff369c1a7979fa54e2573909dc26fa18f7bf4
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-03-15 01:54:43 +0000

    Useless init

--- storage/client/local.py
+++ storage/client/local.py
@@ -13,9 +13,6 @@ from storage.superclass.client import BaseStorageClient
 
 
 class LocalClient(BaseStorageClient):
-    def __init__(self, config: StorageConfig):
-        super().__init__(config)
-
     @property
     def medium(self) -> Medium:
         return Medium.LOCAL
