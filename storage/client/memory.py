bd28bdc4d477cff8014b0d6380258b79b3675584
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-03-20 05:24:52 +0000

    Memory storage implementation

--- storage/client/memory.py
+++ storage/client/memory.py
@@ -1,14 +1,37 @@
 """Memory client for storage."""
+from typing import Dict, List, Tuple
+
 from config.models.env import StorageConfig
 from storage.models.client.medium import Medium
+from storage.models.object.content import ObjectData
+from storage.models.object.models import Object
+from storage.models.object.path import StorageKey
 from storage.superclass.client import BaseStorageClient
 
 
 class MemoryClient(BaseStorageClient):
     def __init__(self, config: StorageConfig):
         super().__init__(config)
-        raise NotImplementedError("Memory client has not been implemented yet")
+        self.storage: Dict[StorageKey, Tuple[Object, ObjectData]] = {}
 
     @property
     def medium(self) -> Medium:
         return Medium.LOCAL
+
+    def get(self, key: StorageKey) -> ObjectData:
+        return self.storage[key][1]
+
+    def stat(self, key: StorageKey) -> Object:
+        return self.storage[key][0]
+
+    def put(self, obj: Object, data: ObjectData) -> None:
+        self.storage[obj.name] = (obj, data)
+
+    def remove(self, key: StorageKey) -> None:
+        self.storage.pop(key)
+
+    def list(self, prefix: StorageKey, recursive: bool = False) -> List[StorageKey]:
+        prefixed = [key for key in self.storage if str(key).startswith(str(prefix.path))]
+        if not recursive:
+            return [key for key in prefixed if key.path.parent == prefix.path]
+        return prefixed
