8f913b66fa21c25ced3f1e0b3741a701f962458a
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-03-31 21:51:44 +0100

    Small config fixes

--- config/constants.py
+++ config/constants.py
@@ -1,3 +1,5 @@
 """Constants for the configuration."""
-CONFIG_FNAME = ".pioneer.json"
-CONFIG_SCHEMA_FNAME = ".pioneer.schema.json"
+import os
+
+CONFIG_PATH = os.environ.get("PIONEER_CONFIG_PATH", ".pioneer.json")
+CONFIG_SCHEMA_PATH = os.environ.get("PIONEER_CONFIG_SCHEMA_FNAME", ".pioneer.schema.json")
--- config/env.py
+++ config/env.py
@@ -4,39 +4,38 @@ from pprint import pprint
 
 from pydantic import ValidationError
 
-from config.constants import CONFIG_FNAME, CONFIG_SCHEMA_FNAME
+from config.constants import CONFIG_PATH, CONFIG_SCHEMA_PATH
 from config.models.env import Config, FormatJSON
 
 
-def make_config(fname=CONFIG_FNAME) -> Config:
+def make_config(path=CONFIG_PATH) -> Config:
     config = Config()
-    with open(fname, "w+", encoding="utf-8") as handle:
+    with open(path, "w+", encoding="utf-8") as handle:
         _json = config.json(**FormatJSON().dict())
         handle.write(_json)
     return config
 
 
-def make_jsonschema(fname=CONFIG_SCHEMA_FNAME, ext="schema.json"):
-    fname = f"{fname}.{ext}"
+def make_jsonschema(path=CONFIG_SCHEMA_PATH):
     config = Config()
-    with open(fname, "w+", encoding="utf-8") as handle:
+    with open(path, "w+", encoding="utf-8") as handle:
         _jsonschema = config.schema_json(**FormatJSON().dict())
         handle.write(_jsonschema)
 
 
-def load_config(fname=CONFIG_FNAME) -> Config:
-    fname = os.environ.get("OBJECTSTASH_CONFIG_PATH", fname)
+def load_config(path=CONFIG_PATH) -> Config:
+    path = os.environ.get("PIONEER_CONFIG_PATH", path)
 
     _env: Config
-    if os.path.isfile(fname):
+    if os.path.isfile(path):
         try:
-            _env = Config.parse_file(fname)
+            _env = Config.parse_file(path)
         except ValidationError as e:
             raise ValidationError(f"Invalid configuration: {e}", Config) from e
         except Exception as e:
             raise RuntimeError(f"Configuration error: {e}") from e
     else:
-        _env = make_config(fname)
+        _env = make_config(path)
     return _env
 
 
