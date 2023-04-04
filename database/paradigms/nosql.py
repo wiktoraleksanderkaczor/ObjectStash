17e8d69e0faab93d02112f3abeb0c74c0002dd3b
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-04-04 23:44:01 +0000

    Implement NoSQL namespaces

--- database/paradigms/nosql.py
+++ database/paradigms/nosql.py
@@ -5,4 +5,14 @@ from database.superclass.client import DatabaseClient
 
 
 class NoSQL(DatabaseClient):
-    pass
+    def namespace(self, name: str) -> "NoSQL":
+        """
+        Create a new namespace in the database.
+
+        Args:
+            name (str): Name of the namespace.
+
+        Returns:
+            NoSQL: A new NoSQL database client.
+        """
+        return NoSQL(self.storage, self.prefix.join(name))
