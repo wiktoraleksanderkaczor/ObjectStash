8b392eebe5ae88708fd3aabbb8afacc5bf6568d0
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-04-17 02:28:02 +0000

    WIP: Indexing database wrapper

---
+++ database/wrapper/index.py
@@ -0,0 +1,27 @@
+"""Index wrapper for database client."""
+from typing import List
+
+from database.models.objects import JSON
+from database.models.query import Query
+from database.superclass.client import DatabaseClient
+from database.wrapper.interface import DatabaseWrapper
+
+
+class IndexWrapper(DatabaseWrapper):
+    def __init__(self, wrapped: DatabaseClient, storage: DatabaseClient, consumers: List):
+        super().__init__(wrapped, consumers)
+        self.index: DatabaseClient = storage
+
+    def insert(self, key: str, value: JSON) -> None:
+        flattened = value.flatten()
+        for item in flattened:
+            k, v = item
+            self.index.insert(repr(k), v)
+        return super().insert(key, value)
+
+    def query(self, query: Query) -> List[JSON]:
+        collections = [self.index.items(repr(field)) for field in query.outputs]
+        items = [item for collection in collections for item in collection]
+        data = [super().get(item) for item in items]
+        data = list(filter(query, data))
+        return data
