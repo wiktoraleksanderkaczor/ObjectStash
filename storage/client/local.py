ad0e71b94e1705aab37cb5f8ccf1beb976e73fc7
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-04-22 20:05:21 +0000

    Fix LocalClient 'Object' persistance

--- storage/client/local.py
+++ storage/client/local.py
@@ -3,9 +3,10 @@ import shutil
 from pathlib import Path
 from typing import List
 
+from config.models.env import StorageConfig
 from storage.models.client.medium import Medium
 from storage.models.object.content import ObjectData
-from storage.models.object.models import Object, ObjectContentInfo
+from storage.models.object.models import Object
 from storage.models.object.path import StorageKey, StoragePath
 from storage.superclass.client import BaseStorageClient
 
@@ -20,15 +21,25 @@ class LocalClient(BaseStorageClient):
             return ObjectData(__root__=handle.read())
 
     def stat(self, key: StorageKey) -> Object:
-        return Object(name=key, content=ObjectContentInfo.from_data(self.get(key)))
+        path = self.meta.joinpath(str(key.path))
+        return Object.parse_file(path)
 
     def put(self, obj: Object, data: ObjectData) -> None:
+        # Define object and data paths
+        meta = self.meta.joinpath(str(obj.name.path))
         path = Path(str(obj.name.path))
+        # Write object to disk
+        meta.parent.mkdir(parents=True, exist_ok=True)
+        meta.touch(exist_ok=True)
+        meta.write_bytes(obj.json().encode())
+        # Create containing directory and write data to disk
         path.parent.mkdir(parents=True, exist_ok=True)
         path.touch(exist_ok=True)
         path.write_bytes(data.__root__)
 
     def remove(self, key: StorageKey) -> None:
+        obj = self.meta.joinpath(str(key.path))
+        shutil.rmtree(obj)
         shutil.rmtree(str(key.path))
 
     def list(self, prefix: StorageKey, recursive: bool = False) -> List[StorageKey]:
@@ -37,3 +48,8 @@ class LocalClient(BaseStorageClient):
         files = [item for item in glob if item.is_file()]
         keys = [StorageKey(storage=self.name, path=StoragePath(str(item))) for item in files]
         return keys
+
+    def __init__(self, config: StorageConfig):
+        super().__init__(config)
+        self.meta = Path(".meta")
+        self.meta.mkdir(parents=True, exist_ok=True)
