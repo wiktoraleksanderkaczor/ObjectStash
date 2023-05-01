d79af449680c8d40cce6530deb01466bb5705002
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-05-01 21:52:24 +0000

    Add LocalClient extended attributes

--- storage/client/local.py
+++ storage/client/local.py
@@ -1,17 +1,20 @@
 """Local storage client."""
 import shutil
 from pathlib import Path
-from typing import List
+from typing import Iterable, List, Union
 
-from config.models.env import StorageConfig
+from xattr import xattr
+
+from datamodel.data import JSON
 from storage.models.client.medium import Medium
 from storage.models.object.file.info import FileData
 from storage.models.object.metadata import Metadata
-from storage.models.object.models import Object
+from storage.models.object.models import File, Folder, Object
 from storage.models.object.path import StorageKey, StoragePath
 from storage.superclass.client import BaseStorageClient
 
 
+# Passthrough permission changes to local filesystem using 'facls'
 class LocalClient(BaseStorageClient):
     @property
     def medium(self) -> Medium:
@@ -22,32 +25,52 @@ class LocalClient(BaseStorageClient):
             return FileData(__root__=handle.read())
 
     def stat(self, key: StorageKey) -> Object:
-        path = self.meta.joinpath(str(key.path))
-        return Object.parse_file(path)
+        attrs = xattr(str(key.path))
+
+        def attr_to_fieldpath(attr: str):
+            attr = attr.removeprefix("pioneer.")
+            split = attr.split(".")
+            parsed = [int(kv) if kv.isnumeric() else kv for kv in split]
+            return parsed
+
+        flattened = [(name, val) for name, val in attrs.items() if isinstance(name, str)]
+        flattened = filter(lambda attr: attr[0].startswith("pioneer."), flattened)
+        flattened = map(lambda attr: (attr_to_fieldpath(attr[0]), attr[1]), flattened)
+        flattened = filter(lambda attr: bool(attr[1]), flattened)
+        flattened = list(flattened)
+        metajson = JSON.from_flattened(flattened)
+        metadata = Metadata.parse_raw(metajson.json())
+        item: Union[File, Folder]
+        try:
+            data = self.get(key)
+            item, _ = File.create(data.__root__)
+        except KeyError:
+            item = Folder()
+        return Object(name=key, metadata=metadata, item=item)
 
     def put(self, obj: Object, data: FileData) -> None:
-        # Define object and data paths
-        meta = self.meta.joinpath(str(obj.name.path))
+        # Define object path
         path = Path(str(obj.name.path))
         # Write object to disk
-        meta.parent.mkdir(parents=True, exist_ok=True)
-        meta.touch(exist_ok=True)
-        meta.write_bytes(obj.json().encode())
-        # Create containing directory and write data to disk
         path.parent.mkdir(parents=True, exist_ok=True)
         path.touch(exist_ok=True)
         path.write_bytes(data.__root__)
+        # Set metadata
+        self.change(obj.name, obj.metadata)
 
     def remove(self, key: StorageKey) -> None:
-        obj = self.meta.joinpath(str(key.path))
-        shutil.rmtree(obj)
         shutil.rmtree(str(key.path))
 
     def change(self, key: StorageKey, metadata: Metadata) -> None:
-        path = self.meta.joinpath(str(key.path))
-        path.unlink()
-        path.touch(exist_ok=False)
-        path.write_bytes(metadata.json().encode())
+        # Handles maps and iterables quite badly
+        flattened = JSON.parse_obj(metadata.dict()).flatten()
+        attrs = xattr(str(key.path))
+        for name, value in flattened:
+            if isinstance(name, Iterable):
+                name = ".".join(name)
+            name = "pioneer." + name
+            attrs.set(name, value)
+            # attrs.set(name, metadata.permissions.json().encode())
 
     def list(self, prefix: StorageKey, recursive: bool = False) -> List[StorageKey]:
         path = Path(str(prefix.path))
@@ -55,8 +78,3 @@ class LocalClient(BaseStorageClient):
         files = [item for item in glob if item.is_file()]
         keys = [StorageKey(storage=self.name, path=StoragePath(str(item))) for item in files]
         return keys
-
-    def __init__(self, config: StorageConfig):
-        super().__init__(config)
-        self.meta = Path(".meta")
-        self.meta.mkdir(parents=True, exist_ok=True)
