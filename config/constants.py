a9190aedcbf14f45aa74b5f8225e08fda0b151ce
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-11-06 21:25:16 +0000

    Delete unused configuration files and modules.

--- config/constants.py
+++
@@ -1,5 +0,0 @@
-"""Constants for the configuration."""
-import os
-
-CONFIG_PATH = os.environ.get("PIONEER_CONFIG_PATH", ".pioneer.json")
-CONFIG_SCHEMA_PATH = os.environ.get("PIONEER_CONFIG_SCHEMA_FNAME", ".pioneer.schema.json")
