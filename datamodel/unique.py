27ef400bcc122bf353057807bbaf54f4e694e760
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-11-07 01:02:28 +0000

    Refactor for UUID (stdlib) instead of UUID4 (Pydantic)

--- datamodel/unique.py
+++ datamodel/unique.py
@@ -1,20 +1,22 @@
 """Custom UUID type for Pioneer.""" ""
 from typing import Optional
-from uuid import uuid4
-
-from pydantic.types import UUID4
+from uuid import UUID, uuid4
 
 
-class UniqueID(UUID4):
+class UniqueID(UUID):
     def __init__(self, value: Optional[str] = None):
-        super().__init__(value or uuid4().hex)
+        super().__init__(hex=value, version=4)
 
     def json(self):
         return self.hex
 
+    @property
+    def hex(self):
+        return str(self)
+
     @classmethod
     def from_json(cls, value):
-        return UUID4(value)
+        return cls(value)
 
     @classmethod
     def random(cls):
